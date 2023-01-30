0830c0147fe4d98124b82724001e1fa67e4bfa25
theadg <deguzmanbernardandrew@gmail.com>
2023-01-30 16:31:41 +0800

    Update capitalize test

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('./capitalize');
 test('Normal Capitalize', () => {
   expect(capitalize('hello')).toBe('Hello');
 });
+
+test('Does not capitalize number', () => {
+  expect(capitalize('1ab')).toBe('1ab');
+});
